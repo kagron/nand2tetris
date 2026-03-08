9cef11529a4880d60f69467d1eddb7483f7d0229
Kyle Grondin <kagron12@gmail.com>
2026-03-08 18:47:34 -0500

    Adding file name to assembly labels

--- compiler/src/jv/codewriter.py
+++ compiler/src/jv/codewriter.py
@@ -11,6 +11,7 @@ class CodeWriter:
     def write_arithmetic(self, command: str):
         self.buffer: list[str] = list()
         self.buffer.append(f"// write_arithmetic {command}\n")
+        file_name_no_ext = self.file.name[:-4].upper()
 
         if command == "add":
             self._pop_d()
@@ -28,53 +29,52 @@ class CodeWriter:
             self.buffer.append("  M=M+1\n")
         elif command == "eq":
             self._pop_d()
-            # TODO: Can I optimize this without loops?
-            # Maybe implement XOR logic to check if equals?
+            # TODO: Can I optimize this without conditions?
             self.buffer.append("  A=M-1\n")
             self.buffer.append("  D=D-M\n")
-            self.buffer.append(f"  @FALSE_EQ_{self.eqi}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_FALSE_EQ_{self.eqi}\n")
             self.buffer.append("  D;JNE\n")
-            self.buffer.append(f"(TRUE_EQ_{self.eqi})\n")
+            self.buffer.append(f"({file_name_no_ext}_TRUE_EQ_{self.eqi})\n")
             self.buffer.append("  D=1\n")
             self._push_d()
-            self.buffer.append(f"  @DONE_EQ_{self.eqi}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_DONE_EQ_{self.eqi}\n")
             self.buffer.append("  0;JMP\n")
-            self.buffer.append(f"(FALSE_EQ_{self.eqi})\n")
+            self.buffer.append(f"({file_name_no_ext}_FALSE_EQ_{self.eqi})\n")
             self.buffer.append("  D=0\n")
             self._push_d()
-            self.buffer.append(f"(DONE_EQ_{self.eqi})\n")
+            self.buffer.append(f"({file_name_no_ext}_DONE_EQ_{self.eqi})\n")
             self.eqi += 1
         elif command == "gt":
             self._pop_d()
             self.buffer.append("  A=M-1\n")
             self.buffer.append("  D=M-D\n")
-            self.buffer.append(f"  @FALSE_GT_{self.gti}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_FALSE_GT_{self.gti}\n")
             self.buffer.append("  D;JLE\n")
-            self.buffer.append(f"(TRUE_GT_{self.gti})\n")
+            self.buffer.append(f"({file_name_no_ext}_TRUE_GT_{self.gti})\n")
             self.buffer.append("  D=1\n")
             self._push_d()
-            self.buffer.append(f"  @DONE_GT_{self.gti}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_DONE_GT_{self.gti}\n")
             self.buffer.append("  0;JMP\n")
-            self.buffer.append(f"(FALSE_GT_{self.gti})\n")
+            self.buffer.append(f"({file_name_no_ext}_FALSE_GT_{self.gti})\n")
             self.buffer.append("  D=0\n")
             self._push_d()
-            self.buffer.append(f"(DONE_GT_{self.gti})\n")
+            self.buffer.append(f"({file_name_no_ext}_DONE_GT_{self.gti})\n")
             self.gti += 1
         elif command == "lt":
             self._pop_d()
             self.buffer.append("  A=M-1\n")
             self.buffer.append("  D=M-D\n")
-            self.buffer.append(f"  @FALSE_LT_{self.lti}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_FALSE_LT_{self.lti}\n")
             self.buffer.append("  D;JGE\n")
-            self.buffer.append(f"(TRUE_LT_{self.lti})\n")
+            self.buffer.append(f"({file_name_no_ext}_TRUE_LT_{self.lti})\n")
             self.buffer.append("  D=1\n")
             self._push_d()
-            self.buffer.append(f"  @DONE_LT_{self.lti}\n")
+            self.buffer.append(f"  @{file_name_no_ext}_DONE_LT_{self.lti}\n")
             self.buffer.append("  0;JMP\n")
-            self.buffer.append(f"(FALSE_LT_{self.lti})\n")
+            self.buffer.append(f"({file_name_no_ext}_FALSE_LT_{self.lti})\n")
             self.buffer.append("  D=0\n")
             self._push_d()
-            self.buffer.append(f"(DONE_LT_{self.lti})\n")
+            self.buffer.append(f"({file_name_no_ext}_DONE_LT_{self.lti})\n")
             self.lti += 1
         elif command == "and":
             self._pop_d()
@@ -141,7 +141,7 @@ class CodeWriter:
             assert index >= 0 and index <= 7, "Index must be between 0-7"
             self._store_d_ram(f"R{5 + index}")
         elif segment == "static":
-            self._store_d_ram(f"{self.file.name[:-4]}_{index}")
+            self._store_d_ram(f"{self.file.name[:-4].upper()}_{index}")
 
     def _push(self, segment: str, index: int):
         self.buffer.append(f"// Push {segment} {index}\n")
@@ -160,7 +160,7 @@ class CodeWriter:
             self._load_ram_d(f"R{5 + index}")
         elif segment == "static":
             # assert index >= 16 and index <= 255, "Index must be between 16-255"
-            self._load_ram_d(f"{self.file.name[:-4]}_{index}")
+            self._load_ram_d(f"{self.file.name[:-4].upper()}_{index}")
 
         self._push_d()
 
